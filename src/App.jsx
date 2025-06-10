49ad8afbafcc58fe6a09bebfb255824b7769acb5
morenoraul <moreno.r.alberto@gmail.com>
2025-06-10 16:00:05 -0300

    Semana 6: Exportación y Experiencia de Usuario

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import ProductList from "./components/ProductList"; // Importamos el componente
 import StatsPanel from "./components/StatsPanel";
 import SearchBar from "./components/SearchBar";
-
+import SortSelect from "./components/SortSelect";
 
 function App() {
   // 1. Estado para almacenar los productos
@@ -15,17 +15,31 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
   //Referencias
   const containerRef = useRef(null);
+  //estados de categorias
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const [categories, setCategories] = useState([]);
+  //ordenamiento de productos
+  const [sortOption, setSortOption] = useState(''); // Nuevo estado para ordenamiento
+  
+
+  
+
+
 
   // 2. useEffect para hacer la petición al montar el componente
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("https://dummyjson.com/products?limit=100");
-        setProducts(response.data.products);
+         const [productsRes, categoriesRes] = await Promise.all([
+          axios.get("https://dummyjson.com/products?limit=100"),
+          axios.get("https://dummyjson.com/products/category-list")
+        ]);
+        setProducts(productsRes.data.products);
+        setCategories(categoriesRes.data);
       } catch (error) {
         console.error("Error al cargar productos:", error);
       } finally {
-        setLoading(false); // Quitamos el estado de carga (éxito o error)
+        setLoading(false);
       }
     };
 
@@ -33,8 +47,11 @@ function App() {
   }, []); // Se ejecuta solo una vez al inicio
 
     //Filtramos los productos obtenidos de la API
-    const filteredProducts = products.filter((p) => p.title.toLowerCase().includes(search.toLowerCase()));
-
+  let filteredProducts = products.filter(
+    (p) =>
+      p.title.toLowerCase().includes(search.toLowerCase()) &&
+      (selectedCategory === "all" || p.category === selectedCategory)
+  );
     //Cantidad de productos en pantalla
     const totalProducts = filteredProducts.length;
 
@@ -53,12 +70,52 @@ function App() {
   : 0;
 // El producto mejor valorado
     const maxRatingObj = filteredProducts.reduce((max, p) => (p.rating > max.rating ? p : max), filteredProducts[0]);
+
+ // precio promedio
+  const promedioPrecio = filteredProducts.length
+  ? (filteredProducts.reduce((sum, p) => sum + p.price, 0) / filteredProducts.length).toFixed(2)
+  : 0;
+ //cantidad de productos
+  const cantidadPorCategoria = selectedCategory === "all"
+  ? filteredProducts.length
+  : filteredProducts.filter(p => p.category === selectedCategory).length;
+// Cantidad de productos con stock > 50
+  const cantidadStockMayor50 = filteredProducts.filter(p => p.stock > 50).length;
+
+// Cantidad de productos con rating > 4.5
+  const cantidadRatingMayor45 = filteredProducts.filter(p => p.rating > 4.5).length;
+  const promedioRating = filteredProducts.length
+  ? (filteredProducts.reduce((sum, p) => sum + p.rating, 0) / filteredProducts.length).toFixed(2)
+  : 0;
+ // precio promedio categoria
+ const promedioPrecioCategoriaFiltrada =
+  selectedCategory !== "all"
+    ? (
+        products
+          .filter((p) => p.category === selectedCategory)
+          .reduce((sum, p) => sum + p.price, 0) /
+        products.filter((p) => p.category === selectedCategory).length || 0
+      ).toFixed(2)
+    : "-";
+
+
     // Funcion Modo Oscuro
     const toggleDarkMode = ()=>{
       setDarkMode(!darkMode);
           containerRef.current.classList.toggle("dark-mode");
    
     };
+
+    //ordenamiento
+    if (sortOption === "price-asc") {
+      filteredProducts = [...filteredProducts].sort((a, b) => a.price - b.price);
+    } else if (sortOption === "price-desc") {
+      filteredProducts = [...filteredProducts].sort((a, b) => b.price - a.price);
+    } else if (sortOption === "rating-asc") {
+      filteredProducts = [...filteredProducts].sort((a, b) => a.rating - b.rating);
+    } else if (sortOption === "rating-desc") {
+      filteredProducts = [...filteredProducts].sort((a, b) => b.rating - a.rating);
+    }
 
   return (
     <div ref={containerRef} className="min-h-screen bg-green-50 p-6">
@@ -89,28 +146,50 @@ function App() {
           </div>
           </div>
         </div>
-          <div className="stats-panel w-full md:w-1/3 bg-white p-4 ">
+          <div className="stats-panel w-full md:w-1/3 bg-white p-4 overflow-auto ">
         <h2 className="text-xl font-bold mb-4">Panel de Estadísticas</h2>
        
 
         <div className="pt-1">
-        <SearchBar search={search} setSearch={setSearch} />
-  <button className="bg-pink-700 rounded p-1 pl-3 pr-3
-    m-0 text-white" onClick={() => setShow(!show)}>{show ? "Ocultar" : "Mostrar"}</button>
-  
-  {show && (  <StatsPanel
-    total={totalProducts}
-    totalPrice={totalPrice}
-    max={maxProductObj.price}
-    maxName={maxProductObj.title}
-    min={minProductObj.price}
-    minName={minProductObj.title}
-    mayor20={mayor20}
-    promedioDescuento={promedioDescuento}
-    maxRatingTitle={maxRatingObj.title}
-    maxRatingValue={maxRatingObj.rating}
-  /> )}
-  {filteredProducts.length == 0 && <div>No se encontraron productos</div>}
+        
+           
+            <button className="bg-pink-700 rounded p-1 pl-3 pr-3
+             m-0 text-white" onClick={() => setShow(!show)}>{show ? "Ocultar" : "Mostrar"}</button>
+             <SearchBar
+          search={search}
+          setSearch={setSearch}
+          categories={categories}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <SortSelect sortOption={sortOption} setSortOption={setSortOption} />
+
+             {/* Renderización condicional */}
+            {show && (  <StatsPanel
+            filteredProducts={filteredProducts}
+            categories={categories}
+            products={products}
+            total={totalProducts}
+            totalPrice={totalPrice}
+            max={maxProductObj.price}
+            maxName={maxProductObj.title}
+            min={minProductObj.price}
+            minName={minProductObj.title}
+            mayor20={mayor20}
+            promedioDescuento={promedioDescuento}
+            maxRatingTitle={maxRatingObj.title}
+            maxRatingValue={maxRatingObj.rating}
+            promedioPrecio={promedioPrecio}
+            cantidadPorCategoria={cantidadPorCategoria}
+            cantidadStockMayor50={cantidadStockMayor50}
+            cantidadRatingMayor45={cantidadRatingMayor45}
+            promedioPrecioCategoriaFiltrada={promedioPrecioCategoriaFiltrada}
+            promedioRating={promedioRating}
+
+            /> )}
+            
+
+            {filteredProducts.length == 0 && <div>No se encontraron productos</div>}
       </div>  
 </div>
         </div>
@@ -119,7 +198,7 @@ function App() {
       {/* Texto adicional */}
       
       <p className="text-center text-gray-400 mt-10 text-sm">
-        Tienda ISPC - 2025 
+        Tienda de Productos - ISPC - RAUL MORENO-  2025 
       </p>
     </div>
     
